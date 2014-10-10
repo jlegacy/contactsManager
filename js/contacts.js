084ab404aa236bb1a5e90bffa5ef3e3885288e0f
jlegacy <jlegacy@suddenlink.net>
2014-10-09 22:55:42 -0400

    Update Working
    Update Working

--- js/contacts.js
+++ js/contacts.js
@@ -1,101 +1,5 @@
 (function () {
 
-	var USER_DEFINED = [{
-			UBusId : 1,
-			UConId : 1,
-			UField : 'TestField1',
-			UItem : 'TestComments',
-			UType : 'date'
-		}, {
-			UBusId : 1,
-			UConId : 1,
-			UField : 'TestField2',
-			UItem : 'william@husker.com',
-			UType : 'text'
-		}, {
-			UBusId : 1,
-			UConId : 2,
-			UField : 'TestField3',
-			UItem : 'TestItem',
-			UType : 'text',
-		}
-	];
-
-	var CONTACTS = [{
-			CBusId : 1,
-			CConId : 1,
-			CFirstName : 'William1',
-			CLastName : 'Test1',
-			CPosition : 'admin',
-			CPhone1 : '0123456789',
-			CPhone2 : '0123456789',
-			CPhone1Fax : 'Y',
-			CPhone2Fax : 'Y',
-			CEmail : 'test1@test.com'
-		}, {
-			CBusId : 2,
-			CConId : 2,
-			CFirstName : 'William2',
-			CLastName : 'Test2',
-			CPosition : 'admin',
-			CPhone1 : '0123456789',
-			CPhone2 : '0123456789',
-			CPhone1Fax : 'Y',
-			CPhone2Fax : 'Y',
-			CEmail : 'test1@test.com'
-		}, {
-			CBusId : 3,
-			CConId : 3,
-			CFirstName : 'William3',
-			CLastName : 'Test3',
-			CPosition : 'admin',
-			CPhone1 : '0123456789',
-			CPhone2 : '0123456789',
-			CPhone1Fax : 'Y',
-			CPhone2Fax : 'Y',
-			CEmail : 'test1@test.com'
-		}
-	];
-
-	var CATEGORIES = [{
-			BBusId : 1,
-			BName : 'test1',
-			BAddress : '1 CanJS Way',
-			BCity : 'Greenville',
-			BState : 'NC',
-			BZip : '27812',
-			BPhone1 : '4444444444',
-			BPhone2 : '5555555555',
-			BPhone1Fax : 'N',
-			BPhone2Fax : 'Y',
-			BEmail : 'test1@husker.com'
-		}, {
-			BBusId : 2,
-			BName : 'test2',
-			BAddress : '1 CanJS Way',
-			BCity : 'Greenville',
-			BState : 'NC',
-			BZip : '27812',
-			BPhone1 : '4444444444',
-			BPhone2 : '5555555555',
-			BPhone1Fax : 'N',
-			BPhone2Fax : 'Y',
-			BEmail : 'test2@husker.com'
-		}, {
-			BBusId : 3,
-			BName : 'test3',
-			BAddress : '1 CanJS Way',
-			BCity : 'Greenville',
-			BState : 'NC',
-			BZip : '27812',
-			BPhone1 : '4444444444',
-			BPhone2 : '5555555555',
-			BPhone1Fax : 'N',
-			BPhone2Fax : 'Y',
-			BEmail : 'test3@husker.com'
-		}
-	];
-
 	/*UserDefined = can.Model.extend({
 	findAll : 'GET /userDefined/{CBusId}{CConId}',
 	find: 'GET /userDefined/{CBusId}{CConId}{UField}'
@@ -104,73 +8,55 @@
 	destroy : "DELETE /userDefined/{CBusId}{CConId}{UField}",
 	id : "U"
 	}, {}); */
-	
-	
-	Contact = can.Model.extend({ 
-			findContacts : function(data, callback)
-			{
-					$.ajax({
-					type: "GET",
-					url: "/ContactsManager/Service1.svc/GetContacts",
-					data: data,
-					contentType: "application/json; charset=utf-8", 
-					dataType: 'json',
-					})
-				.done(function( result ) {
-				    callback(result);
-					});
-				},
-				
-			CreateContactServer: function(data, callback)
-			{
-			//get business id 
-			data.CBusId = can.route.attr('category');
-			//create unique contact id
-			data.CConId = createGuid();
+
+	Contact = can.Model.extend({
+			findContacts : function (data, callback) {
 				$.ajax({
-					type: "POST",
-					url: "/ContactsManager/Service1.svc/AddContact",
-					data: data,
-					contentType: "application/json; charset=utf-8", 
-					dataType: 'json',
-					})
-				.done(function( result ) {
-				    callback(result);
-					});
-			
+					type : "GET",
+					url : "/ContactsManager/Service1.svc/GetContacts",
+					data : data,
+					contentType : "application/json; charset=utf-8",
+					dataType : 'json',
+				})
+				.done(function (result) {
+					callback(result);
+				});
 			},
-			
-			UpdateContactServer: function(data,callback)
-			{
+
+			CreateContactServer : function (data, callback) {
+				//get business id
+				data.CBusId = can.route.attr('category');
+				//create unique contact id
+				data.CConId = createGuid();
 				$.ajax({
-					type: "POST",
-					url: "/ContactsManager/Service1.svc/UpdateContact",
-					data: data,
-					contentType: "application/json; charset=utf-8", 
-					processData: true,
-					dataType: 'json',
-					})
-				.done(function( result ) {
-				    callback(result);
-					});
-				},
+					type : "POST",
+					url : "/ContactsManager/Service1.svc/AddContact",
+					data : data,
+					contentType : "application/json; charset=utf-8",
+					dataType : 'json',
+				})
+				.done(function (result) {
+					callback(result);
+				});
+
+			},
+
 			
-			
-			DeleteContactServer: function(data,callback)
-			{
+
+			DeleteContactServer : function (data, callback) {
 				$.ajax({
-					type: "GET",
-					url: "/ContactsManager/Service1.svc/DeleteContact",
-					data: data,
-					contentType: "application/json; charset=utf-8", 
-					processData: true,
-					dataType: 'json',
-					})
-				.done(function( result ) {
-				    callback(result);
-					});
-				},
-		
+					type : "GET",
+					url : "/ContactsManager/Service1.svc/DeleteContact",
+					data : data,
+					contentType : "application/json; charset=utf-8",
+					processData : true,
+					dataType : 'json',
+				})
+				.done(function (result) {
+					callback(result);
+				});
+			},
+
 		}, {});
 
 	Contact.List = can.Model.List({
@@ -186,196 +72,227 @@
 			},
 			count : function (category) {
 				return this.filter(category).length;
-			},
-			filter : function (contacts) {
-				alert('In Route');
-			},
+			}
+		
 		});
 
 	Category = can.Model.extend({
-			findAll : function(params){
-				return $.get('../ContactsManager/Service1.svc/GetBusiness/', params)
-				},
-			id : 'BBusId'
+			findAllBusiness : function (data, callback) {
+				$.ajax({
+					type : "GET",
+					url : "../ContactsManager/Service1.svc/GetBusiness/",
+					data : data,
+					contentType : "application/json; charset=utf-8",
+					dataType : 'json',
+				})
+				.done(function (result) {
+					callback(result);
+				});
+			}
 		}, {});
 
-	can.fixture('GET /contacts', function () {
-		return [CONTACTS];
-	});
-
-	// create
-	var id = 4;
-	can.fixture("POST /contacts", function () {
-		// just need to send back a new id
-		return {
-		CBusId : 1,
-			CConId : (id++)
-		}
-	});
-
-	// update
-	can.fixture("PUT /contacts/{CConId}", function () {
-		// just send back success
-		return {};
-	});
-
-	// destroy
-	can.fixture("DELETE /contacts/{CConId}", function () {
-		// just send back success
-		return {};
-	});
-
-	can.fixture('GET /categories', function () {
-		return [CATEGORIES];
-	});
-
-	can.route('filter/:category');
-	
 	Contacts = can.Control({
-	
+
 			deleteContact : function (el) {
-			    var data;
+				var data;
 				var x = el.closest('li').find('input[name="CConId"]').val();
-				data = {Contact: x};
-				Contact.DeleteContactServer(data, function(values) {
-				$.growl({ 
-							title: "RoloMax", 
-							message: "Contact Deleted..",
-							style: "notice"});
+				data = {
+					Contact : x
+				};
+				Contact.DeleteContactServer(data, function (values) {
+					$.growl({
+						title : "RoloMax",
+						message : "Contact Deleted..",
+						style : "notice"
+					});
 				});
 				el.closest('li').remove();
 			},
 			updateContact : function (el) {
 				var form = el.closest('form');
 				data = can.deparam(form.serialize());
-				Contact.UpdateContactServer(data, function(values) {
-				$.growl({ 
-							title: "RoloMax", 
-							message: "Contact Updated..",
-							style: "notice"});
+				Contact.UpdateContactServer(data, function (values) {
+					$.growl({
+						title : "RoloMax",
+						message : "Contact Updated..",
+						style : "notice"
+					});
 				});
 			},
 
-			'.contact input change' : function (el, ev) {
-				this.updateContact(el);
-			},
-		
-			'.remove click' : function (el, ev) {
-			  this.deleteContact(el);
-			},
 			
+
+			'.remove click' : function (el, ev) {
+				this.deleteContact(el);
+			},
+
 			'{Contact} created' : function (list, ev, contact) {
 				this.options.contacts.push(contact);
 			}
-			
-		
-			
+
 		});
 
 	Create = can.Control({
-	
-			createMyContacts : function(el)
-			{
-			var form = this.element.find('form');
+
+			createMyContacts : function (el) {
+				var form = this.element.find('form');
 				values = can.deparam(form.serialize());
 				Contact.CreateContactServer(values, function (result) {
-				$('#create').hide();
-				
-				//
-				
-				//
-				
-				
-				'[data-category] click'
-				$.growl({title: "RoloMax", message: "Contact Updated..",style: "notice"});
+				//	$(this).attr('data-category')
+					$('#create').hide();
+					$.growl({
+						title : "RoloMax",
+						message : "Contact Created..",
+						style : "notice"
+					});
 				});
-			},
-			show : function () {
-				this.contact = new Contact();
-				this.element.html(can.view('views/createView.ejs', {
-						contact : this.contact,
-						categories : this.options.categories,
-					}));
-				this.element.slideDown(200);
-			},
-			hide : function () {
-				this.element.slideUp(200);
-			},
-			'{document} #new-contact click' : function () {
-				this.show();
-			},
+			}
+			
+			
+
+			
+		});
+
+	
 		
-			'.save click' : function (el) {
-			//el.attr('Name', 'CConId') = createGuid();
-			this.createMyContacts(el);
-			},
-			'.cancel click' : function () {
-				this.hide();
+
+	function LoadBusiness(data) {
+		var category = can.route.attr('category') || "all";
+		$('#filter').html(can.view('views/filterView.ejs', {
+				categories : data
+			}));
+	}
+
+	
+	
+	
+	
+	function LoadContacts(data) {
+		Contact.findContacts({
+			Business : data
+		}, function (data) {
+			var j = {contacts : data};
+			$('#contacts').html("");
+			$('#contacts').html(can.view('views/contactsList.ejs', {contacts : data}));
+		});
+	}
+	
+	function ShowAddContact() {
+				var x = (can.view('views/createView.ejs', {
+						contact : {},
+						categories : {},
+					}));
+	
+				$('#create').empty();
+				$('#create').hide();
+				$('#create').append(x);
+				$('#create').slideDown();
+				
+			}
+			
+			
+	function HideAddContact () {
+				$('#create').slideUp();
+	}
+
+	$(document).ready(function () {
+
+		$.ajaxPrefilter(function (options, orig, xhr) {
+
+			if (options.processData
+				 && /^application\/json((\+|;).+)?$/i.test(options.contentType)
+				 && /^(post|put|delete)$/i.test(options.type)) {
+				options.data = JSON.stringify(orig.data);
 			}
 		});
-
-	Filter = can.Control({
-			init : function () {
-				var category = can.route.attr('category') || "all";
-				this.element.html(can.view('views/filterView.ejs', {
-						contacts : this.options.contacts,
-						categories : this.options.categories
-					}));
-				this.element.find('[data-category="' + category + '"]').parent().addClass('active');
-			},
-			'[data-category] click' : function (el, ev) {
-				this.element.find('[data-category]').parent().removeClass('active');
-				el.parent().addClass('active');
-				can.route.attr('category', el.data('category'));
-				this.LoadContacts();
-			},
-			
-			LoadContacts : function () {
-			Contact.findContacts({Business:can.route.attr('category')}, function(data) {
-				var j = {contacts : data};
-				$('#contacts').html("");
-				$('#contacts').html(can.view('views/contactsList.ejs', {contacts : data}));
-			});
-			},
 		
+		
+		$("body").delegate(".save", "click", function () {
+			alert('need to call CreateMyContacts');
 		});
 		
-	$(document).ready(function () {
-	
-	$.ajaxPrefilter(function(options, orig, xhr ) { 
-
-  if ( options.processData
-    && /^application\/json((\+|;).+)?$/i.test( options.contentType )
-    && /^(post|put|delete)$/i.test( options.type )
-  ) {       
-    options.data = JSON.stringify( orig.data );
-  }
-}); 
-	
-			Category.findAll({Filter:''}, function (response, request) {
-			if (request === 'success') {
-			var categories = response;
+		$("body").delegate(".cancel", "click", function () {
+			HideAddContact();
+			
+		});
 		
-			new Create('#create', {
-				categories : categories
-			});
-			new Filter('#filter', {
-				contacts : contacts,
-				categories : categories
-			});
-			new Contacts('#contacts', {
-				contacts : contacts,
-			});
+	
+			
+		$("nav").delegate("a", "click", function () {
+			LoadContacts($(this).attr('data-category'));
+		});
+			
+			
+		$("body").delegate("#new-contact", "click", function () {
+			ShowAddContact();
+		});
+		
+		$("body").delegate(".inputEdit", "change", function () {
+			var form = $(this).closest('form');
+				data = can.deparam(form.serialize());
+				UpdateContactServer(data, function (values) {
+					$.growl({
+						title : "RoloMax",
+						message : "Contact Updated..",
+						style : "notice"
+					});
+				});
+		});
+		
+		function UpdateContactServer (data, callback) {
+				$.ajax({
+					type : "POST",
+					url : "/ContactsManager/Service1.svc/UpdateContact",
+					data : data,
+					contentType : "application/json; charset=utf-8",
+					processData : true,
+					dataType : 'json',
+				})
+				.done(function (result) {
+					callback(result);
+				});
+			}
+		
+		
+		//$('contact input change' : function (el, ev) {
+		//		this.updateContact(el);
+		//	},
+		
+		data = {Filter : ''}; 
+		Category.findAllBusiness(data, function(response) {
+		LoadBusiness(response);
+		
+		// update perfect scrollbar
+				jQuery('.scrollbar-vista').scrollbar({
+					"showArrows" : true,
+					"type" : "advanced"
+				});
+		});
 
-			// update perfect scrollbar
-			jQuery('.scrollbar-vista').scrollbar({
-				"showArrows" : true,
-				"type" : "advanced"
-			});
+		Category.findAll({
+			Filter : ''
+		}, function (response, request) {
+			if (request === 'success') {
+
+				
+
+				var categories = response;
+
+				new Create('#create', {
+					categories : categories
+				});
+				new Filter('#filter', {
+					contacts : contacts,
+					categories : categories
+				});
+				new Contacts('#contacts', {
+					contacts : contacts,
+				});
+
+				
 			}
 
 		});
 	});
-	
 
 })();
