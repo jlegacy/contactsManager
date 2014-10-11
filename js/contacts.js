4e624b73abf3ccdf5490ec1a7579f439b6df9ca8
jlegacy <jlegacy@suddenlink.net>
2014-10-10 22:03:21 -0400

    working on user defined
    working on user defined

--- js/contacts.js
+++ js/contacts.js
@@ -9,153 +9,6 @@
 	id : "U"
 	}, {}); */
 
-	Contact = can.Model.extend({
-			findContacts : function (data, callback) {
-				$.ajax({
-					type : "GET",
-					url : "/ContactsManager/Service1.svc/GetContacts",
-					data : data,
-					contentType : "application/json; charset=utf-8",
-					dataType : 'json',
-				})
-				.done(function (result) {
-					callback(result);
-				});
-			},
-
-			CreateContactServer : function (data, callback) {
-				//get business id
-				data.CBusId = can.route.attr('category');
-				//create unique contact id
-				data.CConId = createGuid();
-				$.ajax({
-					type : "POST",
-					url : "/ContactsManager/Service1.svc/AddContact",
-					data : data,
-					contentType : "application/json; charset=utf-8",
-					dataType : 'json',
-				})
-				.done(function (result) {
-					callback(result);
-				});
-
-			},
-
-			
-
-			DeleteContactServer : function (data, callback) {
-				$.ajax({
-					type : "GET",
-					url : "/ContactsManager/Service1.svc/DeleteContact",
-					data : data,
-					contentType : "application/json; charset=utf-8",
-					processData : true,
-					dataType : 'json',
-				})
-				.done(function (result) {
-					callback(result);
-				});
-			},
-
-		}, {});
-
-	Contact.List = can.Model.List({
-			filter : function (category) {
-				this.attr('length');
-				var contacts = new Contact.List([]);
-				this.each(function (contact, i) {
-					if (category === 'all' || category === contact.attr('CBusId')) {
-						contacts.push(contact)
-					}
-				})
-				return contacts;
-			},
-			count : function (category) {
-				return this.filter(category).length;
-			}
-		
-		});
-
-	Category = can.Model.extend({
-			findAllBusiness : function (data, callback) {
-				$.ajax({
-					type : "GET",
-					url : "../ContactsManager/Service1.svc/GetBusiness/",
-					data : data,
-					contentType : "application/json; charset=utf-8",
-					dataType : 'json',
-				})
-				.done(function (result) {
-					callback(result);
-				});
-			}
-		}, {});
-
-	Contacts = can.Control({
-
-			deleteContact : function (el) {
-				var data;
-				var x = el.closest('li').find('input[name="CConId"]').val();
-				data = {
-					Contact : x
-				};
-				Contact.DeleteContactServer(data, function (values) {
-					$.growl({
-						title : "RoloMax",
-						message : "Contact Deleted..",
-						style : "notice"
-					});
-				});
-				el.closest('li').remove();
-			},
-			updateContact : function (el) {
-				var form = el.closest('form');
-				data = can.deparam(form.serialize());
-				Contact.UpdateContactServer(data, function (values) {
-					$.growl({
-						title : "RoloMax",
-						message : "Contact Updated..",
-						style : "notice"
-					});
-				});
-			},
-
-			
-
-			'.remove click' : function (el, ev) {
-				this.deleteContact(el);
-			},
-
-			'{Contact} created' : function (list, ev, contact) {
-				this.options.contacts.push(contact);
-			}
-
-		});
-
-	Create = can.Control({
-
-			createMyContacts : function (el) {
-				var form = this.element.find('form');
-				values = can.deparam(form.serialize());
-				Contact.CreateContactServer(values, function (result) {
-				//	$(this).attr('data-category')
-					$('#create').hide();
-					$.growl({
-						title : "RoloMax",
-						message : "Contact Created..",
-						style : "notice"
-					});
-				});
-			}
-			
-			
-
-			
-		});
-
-	
-		
-
 	function LoadBusiness(data) {
 		var category = can.route.attr('category') || "all";
 		$('#filter').html(can.view('views/filterView.ejs', {
@@ -163,36 +16,194 @@
 			}));
 	}
 
-	
-	
-	
-	
 	function LoadContacts(data) {
-		Contact.findContacts({
+		GetContactServer({
 			Business : data
 		}, function (data) {
-			var j = {contacts : data};
+			var j = {
+				contacts : data
+			};
 			$('#contacts').html("");
-			$('#contacts').html(can.view('views/contactsList.ejs', {contacts : data}));
+			$('#contacts').html(can.view('views/contactsList.ejs', {
+					contacts : data
+				}));
 		});
 	}
-	
+
+	function LoadUserDefined(data) {
+		GetUserServer(data, function (data) {
+			$('#users').html("");
+			$('#users').html(can.view('views/userList.ejs', {
+					user : data
+				}));
+		});
+	}
+
 	function ShowAddContact() {
-				var x = (can.view('views/createView.ejs', {
-						contact : {},
-						categories : {},
-					}));
-	
-				$('#create').empty();
-				$('#create').hide();
-				$('#create').append(x);
-				$('#create').slideDown();
-				
-			}
-			
-			
-	function HideAddContact () {
-				$('#create').slideUp();
+
+		var data = {};
+
+		data.CConId = createGuid();
+		data.CBusId = $('#filter').find('.active').find('a').attr('data-category')
+
+			var x = (can.view('views/createView.ejs', {
+					contact : data,
+					categories : {},
+				}));
+
+		$('#create').empty();
+		$('#create').hide();
+		$('#create').append(x);
+		$('#create').slideDown();
+
+	}
+
+	function HideAddContact() {
+		$('#create').slideUp();
+	}
+
+	function ShowAddBusiness() {
+
+		var data = {};
+
+		data.BBusId = createGuid();
+
+		var x = (can.view('views/createBusiness.ejs', {
+				business : data
+			}));
+
+		$('#createBusiness').empty();
+		$('#createBusiness').hide();
+		$('#createBusiness').append(x);
+		$('#createBusiness').slideDown();
+
+	}
+
+	function HideAddBusiness() {
+		$('#createBusiness').slideUp();
+	}
+
+	function DeleteBusinessServer(data, callback) {
+		$.ajax({
+			type : "GET",
+			url : "/ContactsManager/Service1.svc/DeleteBusiness",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			processData : true,
+			dataType : 'json',
+		})
+		.done(function (result, response) {
+			callback(result, response);
+		});
+	}
+
+	function DeleteContactServer(data, callback) {
+		$.ajax({
+			type : "GET",
+			url : "/ContactsManager/Service1.svc/DeleteContact",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			processData : true,
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
+	}
+
+	function UpdateContactServer(data, callback) {
+		$.ajax({
+			type : "POST",
+			url : "/ContactsManager/Service1.svc/UpdateContact",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			processData : true,
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
+	}
+
+	function CreateContactServer(data, callback) {
+		$.ajax({
+			type : "POST",
+			url : "/ContactsManager/Service1.svc/AddContact",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
+
+	}
+
+	function CreateBusinessServer(data, callback) {
+		$.ajax({
+			type : "POST",
+			url : "/ContactsManager/Service1.svc/AddBusiness",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+		})
+		.done(function (result, response) {
+			callback(result, response);
+		});
+
+	}
+
+	function GetContactServer(data, callback) {
+		$.ajax({
+			type : "GET",
+			url : "/ContactsManager/Service1.svc/GetContacts",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
+	}
+
+	function UpdateBusinessServer(data, callback) {
+		$.ajax({
+			type : "POST",
+			url : "/ContactsManager/Service1.svc/UpdateBusiness",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			processData : true,
+			dataType : 'json',
+		})
+		.done(function (result, response) {
+			callback(result, response);
+		});
+	}
+
+	function findAllBusiness(data, callback) {
+		$.ajax({
+			type : "GET",
+			url : "../ContactsManager/Service1.svc/GetBusiness/",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
+	}
+
+	function GetUserServer(data, callback) {
+		$.ajax({
+			type : "GET",
+			url : "../ContactsManager/Service1.svc/GetUserDefined",
+			data : data,
+			contentType : "application/json; charset=utf-8",
+			dataType : 'json',
+		})
+		.done(function (result) {
+			callback(result);
+		});
 	}
 
 	$(document).ready(function () {
@@ -205,94 +216,145 @@
 				options.data = JSON.stringify(orig.data);
 			}
 		});
-		
-		
-		$("body").delegate(".save", "click", function () {
-			alert('need to call CreateMyContacts');
+
+		$("body").delegate("#contactSave", "click", function () {
+			var form = $('#create').find('form');
+			var values = can.deparam(form.serialize());
+			CreateContactServer(values, function (result) {
+				$('#create').slideUp();
+				LoadContacts($('#filter').find('.active').find('a').attr('data-category'));
+				$.growl({
+					title : "RoloMax",
+					message : "Contact Created..",
+					style : "notice"
+				});
+			});
 		});
-		
-		$("body").delegate(".cancel", "click", function () {
+
+		$("body").delegate("#contactCancel", "click", function () {
 			HideAddContact();
-			
 		});
-		
-	
-			
+
+		$("body").delegate("#businessSave", "click", function () {
+			var form = $('#createBusiness').find('form');
+			var values = can.deparam(form.serialize());
+			CreateBusinessServer(values, function (result, response) {
+				$('#businessCreate').slideUp();
+				HideAddBusiness();
+				$.growl({
+					title : "RoloMax",
+					message : "Business Created..",
+					style : "notice"
+				});
+			});
+		});
+
+		$("body").delegate("#businessCancel", "click", function () {
+			HideAddBusiness();
+		});
+
 		$("nav").delegate("a", "click", function () {
+			$("#section-navigation li").removeClass('active');
+			var x = $(this).closest('li').addClass('active');
 			LoadContacts($(this).attr('data-category'));
 		});
+
+		$('#contacts').delegate("li", "click", function () {
+			var data = {};
+			$("#contacts li").removeClass('active');
+			var x = $(this).addClass('active');
+
+			data.Business = $(this).find('input[name=CBusId').val();
+			data.Contact = $(this).find('input[name=CConId').val();
 			
-			
+			LoadUserDefined(data);
+		});
+
 		$("body").delegate("#new-contact", "click", function () {
 			ShowAddContact();
 		});
-		
-		$("body").delegate(".inputEdit", "change", function () {
+
+		$("body").delegate("#new-business", "click", function () {
+			ShowAddBusiness();
+		});
+
+		$("body").delegate("#contacts .inputEdit", "change", function () {
 			var form = $(this).closest('form');
-				data = can.deparam(form.serialize());
-				UpdateContactServer(data, function (values) {
-					$.growl({
-						title : "RoloMax",
-						message : "Contact Updated..",
-						style : "notice"
-					});
+			data = can.deparam(form.serialize());
+			UpdateContactServer(data, function (values) {
+				$.growl({
+					title : "RoloMax",
+					message : "Contact Updated..",
+					style : "notice"
 				});
-		});
-		
-		function UpdateContactServer (data, callback) {
-				$.ajax({
-					type : "POST",
-					url : "/ContactsManager/Service1.svc/UpdateContact",
-					data : data,
-					contentType : "application/json; charset=utf-8",
-					processData : true,
-					dataType : 'json',
-				})
-				.done(function (result) {
-					callback(result);
-				});
-			}
-		
-		
-		//$('contact input change' : function (el, ev) {
-		//		this.updateContact(el);
-		//	},
-		
-		data = {Filter : ''}; 
-		Category.findAllBusiness(data, function(response) {
-		LoadBusiness(response);
-		
-		// update perfect scrollbar
-				jQuery('.scrollbar-vista').scrollbar({
-					"showArrows" : true,
-					"type" : "advanced"
-				});
+			});
 		});
 
-		Category.findAll({
+		$("body").delegate("#filter .inputEdit", "change", function () {
+			var form = $(this).closest('form');
+			data = can.deparam(form.serialize());
+			UpdateBusinessServer(data, function (values, response) {
+				console.log(response);
+				console.log(values);
+				$.growl({
+					title : "RoloMax",
+					message : "Business Updated..",
+					style : "notice"
+				});
+			});
+		});
+
+		$("body").delegate(".remove", "click", function () {
+			var data;
+
+			var x = $(this).closest('li').find('input[name="CConId"]').val();
+			var y = $(this).closest('li');
+			data = {
+				Contact : x
+			};
+			DeleteContactServer(data, function (values) {
+				y.remove();
+				$.growl({
+					title : "RoloMax",
+					message : "Contact Deleted..",
+					style : "notice"
+				});
+			});
+		});
+
+		$("body").delegate(".businessRemove", "click", function () {
+			var data;
+
+			var x = $(this).closest('li').find('input[name="BBusId"]').val();
+			var y = $(this).closest('li');
+			data = {
+				Business : x
+			};
+			DeleteBusinessServer(data, function (values, response) {
+				console.log(values, response);
+				y.remove();
+				$.growl({
+					title : "RoloMax",
+					message : "Business Deleted..",
+					style : "notice"
+				});
+			});
+		});
+
+		data = {
 			Filter : ''
-		}, function (response, request) {
-			if (request === 'success') {
+		};
+		findAllBusiness(data, function (response) {
 
-				
+			LoadBusiness(response);
 
-				var categories = response;
-
-				new Create('#create', {
-					categories : categories
-				});
-				new Filter('#filter', {
-					contacts : contacts,
-					categories : categories
-				});
-				new Contacts('#contacts', {
-					contacts : contacts,
-				});
-
-				
-			}
-
+			// update perfect scrollbar
+			jQuery('.scrollbar-vista').scrollbar({
+				"showArrows" : true,
+				"type" : "advanced"
+			});
 		});
+
 	});
 
 })();
